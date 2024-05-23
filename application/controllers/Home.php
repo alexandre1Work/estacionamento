709b3e837a22078d4f80e33d1da875749015df38
alexandre1Work <alexandre.velosowork@gmail.com>
2024-05-22 22:48:11 -0300

    fatiamento em: footer, header, navbar, sidebar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,9 @@ defined('BASEPATH') or exit('Ação não permitida');
 class Home extends CI_Controller{
 
     public function index(){
+
+        $this->load->view('layout/header');
         $this->load->view('home/index');
+        $this->load->view('layout/footer');
     }
 }
